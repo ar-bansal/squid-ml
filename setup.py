5381cc8b4ca2458f43802973cea381e3a46af6f4
Aryan Bansal <aryanbansal1710@gmail.com>
2025-01-07 22:23:26 -0800

    change src to mlops

--- setup.py
+++ setup.py
@@ -1,3 +1,36 @@
+# from setuptools import setup, find_packages
+
+# setup(
+#     name="mlops",
+#     version="0.1.0",
+#     description="MLOps tools and utilities",
+#     author="Aryan Bansal",
+#     author_email="aryanbansal1710@gmail.com",
+#     url="https://github.com/ar-bansal/mlops",
+#     package_dir={"": "src"},  # Root of the packages is the src directory
+#     packages=find_packages(where="src"),  # Automatically find all packages under src
+#     include_package_data=True,  # Includes non-Python files specified in MANIFEST.in
+#     install_requires=[
+#         "mlflow",
+#         "boto3"
+#     ],
+#     entry_points={
+#         "console_scripts": [
+#             "mlops=src.cli_entrypoint:main",  # Enables CLI functionality
+#         ],
+#     },
+#     package_data={
+#         "infra": ["infra/*", "infra/minio/*", "infra/mlflow/*", "infra/postgres/*"],  # Explicitly include infra files
+#     },
+#     classifiers=[
+#         "Programming Language :: Python :: 3",
+#         "License :: OSI Approved :: MIT License",
+#         "Operating System :: OS Independent",
+#     ],
+#     python_requires=">=3.9",
+# )
+
+
 from setuptools import setup, find_packages
 
 setup(
@@ -7,8 +40,8 @@ setup(
     author="Aryan Bansal",
     author_email="aryanbansal1710@gmail.com",
     url="https://github.com/ar-bansal/mlops",
-    package_dir={"": "src"},  # Root of the packages is the src directory
-    packages=find_packages(where="src"),  # Automatically find all packages under src
+    # package_dir={"": "src"},  # Root of the packages is the src directory
+    packages=find_packages(),  # Automatically find all packages under src
     include_package_data=True,  # Includes non-Python files specified in MANIFEST.in
     install_requires=[
         "mlflow",
@@ -16,8 +49,11 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "mlops=src.cli_entrypoint:main",  # Enables CLI functionality
+            "mlops=mlops.cli_entrypoint:main",  # Enables CLI functionality
         ],
+    },
+    package_data={
+        "infra": ["infra/*", "infra/minio/*", "infra/mlflow/*", "infra/postgres/*"],  # Explicitly include infra files
     },
     classifiers=[
         "Programming Language :: Python :: 3",
