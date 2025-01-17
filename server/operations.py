e1838be3886e50bcb424117d046aaaa997970ab2
Aryan Bansal <aryanbansal1710@gmail.com>
2025-01-16 17:38:24 -0800

    change container names to <project-name>-mlops-<service-type>, where service-type = {ui, artifact_store, backend_store}

--- server/operations.py
+++ server/operations.py
@@ -24,7 +24,7 @@ def _set_ports(ui_port, artifact_store_port, console_port):
     os.environ["CONSOLE_PORT"] = str(console_port)
 
 
-def _set_versions(python_: str="", mlflow_: str=""):
+def _set_versions(python_: str, mlflow_: str):
     # Validate version format for Python and MLflow
     if python_ and not bool(re.match(r"^\d+\.\d+$", python_)):
         raise ValueError(f"Python version must be of the form '<major>.<minor>', like '3.10'. Provided '{python_}'")
@@ -35,21 +35,37 @@ def _set_versions(python_: str="", mlflow_: str=""):
     os.environ["MLFLOW_VERSION"] = mlflow_
 
 
+def _set_container_names(ui, backend_store, artifact_store):
+    os.environ["UI_CONTAINER_NAME"] = ui
+    os.environ["BACKEND_STORE_CONTAINER_NAME"] = backend_store
+    os.environ["ARTIFACT_STORE_CONTAINER_NAME"] = artifact_store
+
+
 class Server:
-    def __init__(self, ui_port=5001, artifact_store_port=5002, console_port=5003):
+    def __init__(self, project_name=None, ui_port=5001, artifact_store_port=5002, console_port=5003):
+        if not project_name:
+            project_name = os.path.basename(os.getcwd())
+
+        self.project_name = project_name
         self.ui_port = ui_port
         self.artifact_store_port = artifact_store_port
         self.console_port = console_port
         mlflow.set_tracking_uri(f"http://localhost:{self.ui_port}")
 
 
-    def start(self, quiet=True, python_version=None, mlflow_version=None):
+    def start(self, quiet=True, python_version="", mlflow_version=""):
         docker = create_client()
         _set_project_dir(os.getcwd())
+        container_names = {
+            "ui": f"{self.project_name}-mlops-ui", 
+            "backend_store": f"{self.project_name}-mlops-backend_store", 
+            "artifact_store": f"{self.project_name}-mlops-artifact_store"
+        }
+        _set_container_names(**container_names)
         _set_ports(self.ui_port, self.artifact_store_port, self.console_port)
+        _set_versions(python_=python_version, mlflow_=mlflow_version)
 
         if python_version and mlflow_version:
-            _set_versions(python_=python_version, mlflow_=mlflow_version)
             docker.compose.build(quiet=quiet)
         elif python_version or mlflow_version:
             argument = "python_version" if python_version else "mlflow_version"
