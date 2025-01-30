75920b52c6a6930026f7309c531d8d624d5fd320
Aryan Bansal <aryanbansal1710@gmail.com>
2025-01-29 19:17:44 -0800

    clean up

--- tests/test_loggers.py
+++ tests/test_loggers.py
@@ -21,12 +21,10 @@ def infra_setup_and_teardown():
     )
 
     server.start()
-    # print("Started")
 
     yield
 
     server.down(delete_all_data=True)
-    # print("stopped")
 
 
 @pytest.fixture
@@ -43,7 +41,8 @@ def pytorch_logger_save_graph():
     return PyTorchLogger(save_graph=True, logging_kwargs={"log_models": True})
 
 
-def dummy_train_function(model, x, y, *args, **kwargs):
+###################################### TRAIN FUNCTIONS ######################################
+def dummy_train_function_sklearn(model, x, y, *args, **kwargs):
     model.fit(x, y)
 
     return model, {'accuracy': 0.95}  # Dummy model and metrics
@@ -63,42 +62,49 @@ def dummy_train_function_pytorch(model, datamodule, *args, **kwargs):
     }
 
 
+###################################### SKLEARN TESTING ######################################
+def test_sklearn_logger_log_invalid(sklearn_logger):
+    model = LinearRegression()
+    x = np.random.rand(10, 10)
+    y = np.random.rand(10, 1)
+
+    logged_func = sklearn_logger.log(dummy_train_function_sklearn)
+    with pytest.raises(ValueError, match=f"experiment_name must be specified as a kwarg when calling dummy_train_function_sklearn."):
+        model, metrics = logged_func(model, x, y)
+
+        
 def test_sklearn_logger_log(sklearn_logger):
     # Log using the sklearn_logger
     model = LinearRegression()
     x = np.random.rand(10, 10)
     y = np.random.rand(10, 1)
 
-    logged_func = sklearn_logger.log(dummy_train_function)
+    logged_func = sklearn_logger.log(dummy_train_function_sklearn)
     model, metrics = logged_func(model, x, y, experiment_name='test_sklearn')
     
     # Ensure the correct metrics were returned
     assert metrics['accuracy'] == 0.95
 
-def test_sklearn_logger_invalid(sklearn_logger):
-    model = LinearRegression()
-    x = np.random.rand(10, 10)
-    y = np.random.rand(10, 1)
 
-    logged_func = sklearn_logger.log(dummy_train_function)
-    with pytest.raises(ValueError, match=f"experiment_name must be specified as a kwarg when calling dummy_train_function."):
-        model, metrics = logged_func(model, x, y)
+def test_mlflow_run_sklearn_logger():
+    # Get the latest run
+    client = MlflowClient(mlflow.get_tracking_uri())
+    
+    # Get the last completed run
+    latest_run = client.search_runs(experiment_ids=[1])[0].to_dictionary()
+    print(latest_run)
+    artifact_uri = latest_run["info"]["artifact_uri"]
+    model = mlflow.sklearn.load_model(artifact_uri + "/model")
 
-
-def test_pytorch_logger1_log(pytorch_logger):
-    model = NeuralNetwork()
-    datamodule = TensorDataModule(
-        X=torch.rand((20, 10)), 
-        y=torch.randint(0, 2, (20, ))
-    )
-
-    logged_func = pytorch_logger.log(dummy_train_function_pytorch)
-    model, metrics = logged_func(model, datamodule, experiment_name="test_pytorch")     
-
-    assert metrics["accuracy"] == 0.95
+    # Ensure the run is successful
+    status = latest_run["info"]["status"]
+    assert status == "FINISHED"
+    # Assert that the model artifact exists
+    assert type(model)  == LinearRegression    
 
 
-def test_pytorch_logger2_log_invalid(pytorch_logger_save_graph):
+###################################### PYTORCH TESTING ######################################
+def test_pytorch_logger_save_graph_log_invalid(pytorch_logger_save_graph):
     model = NeuralNetwork()
     datamodule = TensorDataModule(
         X=torch.rand((20, 10)), 
@@ -114,26 +120,17 @@ def test_pytorch_logger2_log_invalid(pytorch_logger_save_graph):
         model, metrics = logged_func(model, datamodule, experiment_name="test_pytorch")
 
 
+def test_pytorch_logger_log(pytorch_logger):
+    model = NeuralNetwork()
+    datamodule = TensorDataModule(
+        X=torch.rand((20, 10)), 
+        y=torch.randint(0, 2, (20, ))
+    )
 
-def test_mlflow_run_sklearn_logger():
-    # # Start the server
-    # server.start()
+    logged_func = pytorch_logger.log(dummy_train_function_pytorch)
+    model, metrics = logged_func(model, datamodule, experiment_name="test_pytorch")     
 
-    # Get the latest run
-    client = MlflowClient(mlflow.get_tracking_uri())
-    
-    # Get the last completed run
-    latest_run = client.search_runs(experiment_ids=[1])[0].to_dictionary()
-    print(latest_run)
-    artifact_uri = latest_run["info"]["artifact_uri"]
-    model = mlflow.sklearn.load_model(artifact_uri + "/model")
-
-    # Ensure the run is successful
-    status = latest_run["info"]["status"]
-    assert status == "FINISHED"
-    # Assert that the model artifact exists
-    assert type(model)  == LinearRegression
-
+    assert metrics["accuracy"] == 0.95
 
 
 def test_mlflow_run_pytorch_logger():
@@ -147,19 +144,3 @@ def test_mlflow_run_pytorch_logger():
     status = latest_run["info"]["status"]
     assert status == "FINISHED"
     assert type(model) == NeuralNetwork
-
-
-
-# def test_mlflow_run_pytorch_logger2_invalid():
-#     client = MlflowClient(mlflow.get_tracking_uri())
-
-#     latest_run = client.search_runs(experiment_ids=[2])[1].to_dictionary()
-#     artifact_uri = latest_run["info"]["artifact_uri"]
-
-#     model = mlflow.pytorch.load_model(artifact_uri + "/model")
-
-#     status = latest_run["info"]["status"]
-#     assert status == "FINISHED"
-#     assert type(model) == NeuralNetwork
-
-
